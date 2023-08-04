37257f2b1fd773be33529fabf6323cf74310ce61
salamanderman234 <tresnasaputra9@gmail.com>
2023-08-04 17:02:02 +0800

    menambahkan layout admin

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -9,14 +9,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>RME - @yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="{{ asset('assets/css/layout.css') }}" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
-<body class="bg-secondary">
-
-    @yield('content')
+<body class="bg-secondary sb-nav-fixed">
+    @include('layout/components/navbar')
+    <div id="layoutSidenav">
+        @include('layout/components/sidebar')
+        <div id="layoutSidenav_content" class="bg-white">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Dashboard</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                    @yield('content')
+                </div>
+            </main>
+            @include('layout/components/footer')
+        </div>    
+    </div>
 
     <script src="{{ asset('assets/js/jquery.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/fontawesome/js/all.min.js') }}"></script>
+    <script src="{{ asset('assets/js/layout.js') }}"></script>
+    <script src="{{ asset('assets/js/datatables-simple-demo.js') }}"></script>
+    <script src="{{ asset('assets/js/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('assets/js/chart-bar-demo.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
 
     @yield('specific-js')
 </body>
